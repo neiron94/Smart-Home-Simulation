797933337b68c1962a7af3ac407ec3e1811493f6
Ales Shvaibovich <neironis94@gmail.com>
2023-12-30 15:51:36 +0100

    Design pattern Prototype is added and implemented in RoomConfiguration

--- src/main/java/place/RoomConfiguration.java
+++ src/main/java/place/RoomConfiguration.java
@@ -1,7 +1,68 @@
 package place;
 
 
+import utils.HelpFunctions;
+import utils.Prototype;
+
 import java.awt.Color;
 
-public record RoomConfiguration(String name, float temperature, int humidity, int brightness, Color color) {
+public class RoomConfiguration implements Prototype {
+    private String name;
+    private float temperature;
+    private int humidity;
+    private int brightness;
+    private Color color;
+
+    public RoomConfiguration(String name, float temperature, int humidity, int brightness, Color color) {
+        this.name = name;
+        this.temperature = temperature;
+        this.humidity = HelpFunctions.adjustPercent(humidity);
+        this.brightness = HelpFunctions.adjustPercent(brightness);
+        this.color = new Color(color.getRGB());
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public float getTemperature() {
+        return temperature;
+    }
+
+    public int getHumidity() {
+        return humidity;
+    }
+
+    public int getBrightness() {
+        return brightness;
+    }
+
+    public Color getColor() {
+        return new Color(color.getRGB());
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setTemperature(float temperature) {
+        this.temperature = temperature;
+    }
+
+    public void setHumidity(int humidity) {
+        this.humidity = HelpFunctions.adjustPercent(humidity);
+    }
+
+    public void setBrightness(int brightness) {
+        this.brightness = HelpFunctions.adjustPercent(brightness);
+    }
+
+    public void setColor(Color color) {
+        this.color = new Color(color.getRGB());
+    }
+
+    @Override
+    public RoomConfiguration copy() {
+        return new RoomConfiguration(name, temperature, humidity, brightness, color);
+    }
 }
