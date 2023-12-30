d8229bcd1371de911759577b0b84cfed12d01bba
Artem Liamkin <artemlyamkin@yahoo.com>
2023-12-31 00:12:39 +0100

    Implemented static block in EntertainmentService to generate playlists

--- src/main/java/consumer/device/common/entertainment/EntertainmentService.java
+++ src/main/java/consumer/device/common/entertainment/EntertainmentService.java
@@ -2,19 +2,24 @@ package consumer.device.common.entertainment;
 
 import utils.ConfigurationReader;
 import utils.HelpFunctions;
-
-import java.util.Random;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class EntertainmentService {
 
     static {
         ConfigurationReader.readContentConfig();
+
+        for (int i = 0; i < new Random().nextInt(3) + 3; ++i) { // Randomly generate playlists
+            List<Song> playlist = new ArrayList<>();
+            for (int j = 0; j < new Random().nextInt(3) + 5; ++j) playlist.add(AudioService.getRandomSong());
+            AudioService.addPlaylist(playlist);
+        }
     }
 
     public static class GameService {
-        private final static List<Game> games = new ArrayList<>(); // simulation of database of games TODO - should be preloaded
+        private final static List<Game> games = new ArrayList<>(); // Simulation of database with games
 
         public static void addGame(Game game) {
             games.add(game);
@@ -26,8 +31,8 @@ public class EntertainmentService {
     }
 
     public static class AudioService {
-        private final static List<Song> songs = new ArrayList<>(); // simulation of database of songs  TODO - should be preloaded
-        private final static List<List<Song>> playlists = new ArrayList<>(); // simulation of database of playlists TODO - should be precreated
+        private final static List<Song> songs = new ArrayList<>(); // Simulation of database with songs
+        private final static List<List<Song>> playlists = new ArrayList<>(); // Simulation of database with playlists
 
         public static void addSong(Song song) {
             songs.add(song);
@@ -38,7 +43,7 @@ public class EntertainmentService {
         }
 
         public static void addPlaylist(List<Song> playlist) {
-            // TODO
+            playlists.add(playlist);
         }
 
         public static List<Song> getRandomPlaylist() {
@@ -47,7 +52,7 @@ public class EntertainmentService {
     }
 
     public static class VideoService {
-        private final static List<Video> shows = new ArrayList<>();    // simulation of database of videos TODO - should be preloaded
+        private final static List<Video> shows = new ArrayList<>(); // Simulation of database with videos
 
         public static void addVideo(Video video) {
             shows.add(video);
