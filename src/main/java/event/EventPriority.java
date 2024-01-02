f1b135cb96912726e857a70494607223e80919e0
Artem Liamkin <artemlyamkin@yahoo.com>
2024-01-02 01:28:09 +0100

    Completed event enums

--- src/main/java/event/EventPriority.java
+++ src/main/java/event/EventPriority.java
@@ -1,7 +1,17 @@
 package event;
 
 public enum EventPriority {
-    LOW,
-    MEDIUM,
-    HIGH
+    LOW("Low"),
+    MEDIUM("Medium"),
+    HIGH("High");
+
+    private final String description;
+
+    EventPriority(String description) {
+        this.description = description;
+    }
+
+    public String toString() {
+        return String.format("%s priority", description);
+    }
 }
