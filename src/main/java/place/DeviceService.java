ee15287be6e48a5a2688886e76b72a2ca7805de2
Artem Liamkin <artemlyamkin@yahoo.com>
2024-01-07 12:01:42 +0100

    DeviceService deleted

--- src/main/java/place/DeviceService.java
+++
@@ -1,9 +0,0 @@
-package place;
-
-import consumer.device.Device;
-
-public class DeviceService {
-    public void takeDevice(Device device, Room room) {
-        // TODO Implement after implementing Device.repair()
-    }
-}
