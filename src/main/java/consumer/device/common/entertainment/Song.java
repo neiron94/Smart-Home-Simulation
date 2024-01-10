6e1ca39fb8189d2d1020fc4c5238a8e87eb5ac54
Ales Shvaibovich <neironis94@gmail.com>
2024-01-10 16:58:41 +0100

    JavaDocs for entertainment package are done

--- src/main/java/consumer/device/common/entertainment/Song.java
+++ src/main/java/consumer/device/common/entertainment/Song.java
@@ -2,4 +2,12 @@ package consumer.device.common.entertainment;
 
 import java.time.Duration;
 
+/**
+ * Song for stereo system.
+ * @param author Author of the song.
+ * @param album Album of the song.
+ * @param name Name of the song.
+ * @param genre Genre of the song.
+ * @param duration Duration of the song.
+ */
 public record Song(String author, String album, String name, SongGenre genre, Duration duration) {}
