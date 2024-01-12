07d1665923890c5fe10d5900faaf4578500d472b
Artem Liamkin <artemlyamkin@yahoo.com>
2024-01-12 15:37:51 +0100

    Added TODO with bug description

--- src/main/java/creature/Creature.java
+++ src/main/java/creature/Creature.java
@@ -68,7 +68,7 @@ public abstract class Creature {
 
         boolean[] canDoAction = {true}; // Array for changing value in stream
         memory.forEach(queue -> {
-                    if (queue.peek().getDuration().equals(Duration.ZERO) && canDoAction[0]) { // Action can be performed
+                    if (queue.peek().getDuration().equals(Duration.ZERO) && canDoAction[0]) { // Action can be performed // TODO Bug - Doing actions when busy by other actions
                         if (queue.poll().perform()) { // Successful perform
                             canDoAction[0] = false; // Can do only one action per simulation tick
                             isBusy = !queue.isEmpty() && queue.peek().isBusy();
