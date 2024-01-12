8cf15961fe7b5244a22cabf859307c0a20b04022
Ales Shvaibovich <neironis94@gmail.com>
2024-01-12 14:44:08 +0100

    Javadocs for smarthome package are done

--- src/main/java/smarthome/SmartHome.java
+++ src/main/java/smarthome/SmartHome.java
@@ -1,6 +1,14 @@
 package smarthome;
 
+/**
+ * Main class, where smart home simulation starts.
+ */
 public class SmartHome {
+
+    /**
+     * Main function, which calls main cycle of simulation ({@link Simulation#simulate()}).
+     * @param args arguments of main function
+     */
     public static void main(String[] args) {
         Simulation.getInstance().simulate();
     }
